d0e997aa3359d1795f9990ea7efe636dbcb7dbc9
zarifbek-yuldashmatov-230251 <161605979+zarifbek-yuldashmatov-230251@users.noreply.github.com>
2025-09-02 15:51:59 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,24 @@
+
+
 #include <iostream>
 using namespace std;
 
-float minimum(float a, float b, float c){
-    if (a<b && a<c){
-        return a;
-    } else if(b<c){
-        return b;
-    } else return c;
-}
-
-float maximum(float a, float b, float c) {
-    if (a>b && a>c) {
-        return a;}
-    else if (a<b && b>c){
-        return b;
-    } else return c;
-}
-
-float median(float a, float b, float c){
-    if (a>b && c>b && a<c) {
-        return a;
-    } else if (b>a && a>c){
-        return b;
-    } else return c;
-}
 int main() {
-    float a,b,c;
-    cin>>a>>b>>c;
-    //cout<<minimum(a, b, c)<<endl;
-    //cout<<maximum(a, b, c)<<endl;
-    cout<<median(a, b, c)<<endl;
+    int n;
+    cin>>n;
+    int a=0, b=1;
+    if(n==1){
+        cout<<a<<" ";
+    }
+    if(n==2 || n>2){
+        cout<<a<<" "<<b<<endl;
+    }
+    int c;
+    for(int i=3;i<=n;i++){
+        c=a+b;
+        cout<<c<<" ";
+        a=b;
+        b=c;
+    }
     return 0;
 }
